277ef1fb2b39e290a4be76cd0475708fb101b207
KarmaQP <quiteplacedev@gmail.com>
2022-04-16 17:23:22 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd5bb436f-c1e9-43cc-b9db-cac76af632d6';
+export const MODAL_CLOSE_SEC = 2.5;
